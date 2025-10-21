a0fd7dc9cf590fc4474a6797ab86def756bdd042
ayushschaudhari1904@gmail.com <ayushschaudhari1904@gmail.com>
2025-10-21 16:41:24 +0530

    chore: command line arguments

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ import (
 //go:embed etc
 var defaultConfigDir embed.FS
 
+// TODO: refactor
 func main() {
 	lv := &libvirtapi.Libvirt{}
 	err := lv.Init()
@@ -31,9 +32,33 @@ func main() {
 	}
 	switch args[1] {
 	case "setup":
-		setup.Init(defaultConfigDir)
+		if len(args) < 3 {
+			fmt.Println("Please provide an argument for setup. Valid arguments:")
+			fmt.Println("1. config")
+			fmt.Println("2. images")
+			return
+		}
+		switch args[2] {
+		case "config":
+			setup.Init(defaultConfigDir)
+		case "images":
+			// TODO: image setup
+		default:
+			fmt.Println("Please provide a valid argument for setup. Valid arguments:")
+			fmt.Println("1. config")
+			fmt.Println("2. images")
+			return
+
+		}
 	case "serve":
 		serve(app)
+	default:
+		if len(args) < 2 {
+			fmt.Println("Please provide a valid argument. Valid arguments:")
+			fmt.Println("1. setup")
+			fmt.Println("2. serve")
+			return
+		}
 
 	}
 
